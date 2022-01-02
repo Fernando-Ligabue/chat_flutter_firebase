f747b55289038203355feba4e026e8a5b9edf264
Fernando-Ligabue <76406175+Fernando-Ligabue@users.noreply.github.com>
2022-01-02 11:42:58 +0000

    implementation of push notification to firebase complete, foreground, background and terminated states ok.

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,10 +1,12 @@
 import 'package:chat_firestore/core/models/chat_user.dart';
 import 'package:chat_firestore/core/services/auth/auth_service.dart';
+import 'package:chat_firestore/core/services/notifications/chat_notification_service.dart';
 import 'package:chat_firestore/pages/auth_page.dart';
 import 'package:chat_firestore/pages/chat_page.dart';
 import 'package:chat_firestore/pages/loading_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class AuthOrAppPage extends StatelessWidget {
 //criado método que inicializa a app firebase e bd do firestore e realtime
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
